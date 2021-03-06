b4e6ed665b840a7beb94698b00d9644eae572eaa
Thom Konan <thomkonan@gmail.com>
2021-03-06 16:32:04 -0500

    API works

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,38 +1,36 @@
 class Api::BooksController < ApplicationController
-def index
-  @books = Book.all
-  render "index.json.jb"
-end
+  def index
+    @books = Book.all
+    render "index.json.jb"
+  end
 
-def create
-  @book = Book.new(
-  title: params[:title]
-  author: params[:author]
-  year: params[:year]
-  )
-  @book.save
-  render "show.json.jb"
-end
+  def create
+    @book = Book.new(
+      title: params[:title],
+      author: params[:author],
+      year: params[:year],
+    )
+    @book.save
+    render "show.json.jb"
+  end
 
-def show
-  @book = Book.find_by(id: params[:id])
-  render "show.json.jb"
-end
+  def show
+    @book = Book.find_by(id: params[:id])
+    render "show.json.jb"
+  end
 
-def update
-  @book = Book.find_by(id: params[:id])
-  @book.title = params[:title] || book.title
-  @book.author = params[:author] || book.author
-  @book.year = params[:year] || book.year
-  @book.save
-  render "show.json.jb"
-end
+  def update
+    @book = Book.find_by(id: params[:id])
+    @book.title = params[:title] || book.title
+    @book.author = params[:author] || book.author
+    @book.year = params[:year] || book.year
+    @book.save
+    render "show.json.jb"
+  end
 
-def destroy
-  @book = Book.find_by(id: params[:id])
-  @book.destroy
-  render json: { message: "Book has been successfully destroyed." }
-end
-
-
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    @book.destroy
+    render json: { message: "Book has been successfully destroyed." }
+  end
 end
