164823f9689edf39d6a9393d0c4c55364377865c
Thom Konan <thomkonan@gmail.com>
2021-03-06 16:14:35 -0500

    added json

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,2 +1,37 @@
 class Api::BooksController < ApplicationController
+def index
+  @books = Book.all
+  render "index.json.jb"
+end
+
+def create
+  @book = Book.new(
+  title: params[:title]
+  author: params[:author]
+  year: params[:year]
+  )
+  @book.save
+  render "show.json.jb"
+end
+
+def show
+  @book = Book.find_by(id: params[:id])
+  render "show.json.jb"
+end
+
+def update
+  @book = Book.find_by(id: params[:id])
+  @book.title = params[:title] || book.title
+  @book.author = params[:author] || book.author
+  @book.year = params[:year] || book.year
+  @book.save
+  render "show.json.jb"
+end
+
+def destroy
+  @book = Book.find_by(id: params[:id])
+  @book.destroy
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/books" => "books#index"
+    post "/books" => "books#create"
+    get "/books" => "books#show"
+    patch "/books" => "books#update"
+    destroy "/books" => "books#delete"
+  end
 end
